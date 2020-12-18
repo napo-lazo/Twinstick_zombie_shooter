6605419a880b90c06e2fb2b571e4646970365fec
napo-lazo <napoleon.lazo@hotmail.com>
2020-12-17 21:38:57 -0600

    Implemented basic player movement and mouse following

---
+++ Assets/Scripts/LookAtCursor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCursor : MonoBehaviour {
+
+    Vector3 mousePos;
+
+    void Update() {
+        mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        transform.up = new Vector2(
+                                    mousePos.x - transform.position.x,
+                                    mousePos.y - transform.position.y
+                                   );
+
+    }
+}
